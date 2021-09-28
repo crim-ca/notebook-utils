6f719a20d109def612fb5dfedecfef4014d2d81c
Antoine Daigneault-Demers <antoine.daigneault-demers@crim.ca>
2021-09-28 17:02:16 -0400

    Transfered notebook stripping functions

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+SRC_DIR = "src"
+
+setup(
+    name="notebook_utils",
+    description="Functions and utilities for machine learning and data science.",
+    author="Computer Research Institute of Montreal (CRIM)",
+    version="0.0.1",
+    packages=find_packages(where=SRC_DIR, exclude=["tests"]),
+    package_dir={"": SRC_DIR},
+    entry_points={
+        "console_scripts": [
+            "crim_notebooks_are_stripped = notebook_utils.automation:notebooks_are_stripped_cli"
+        ]
+    },
+)
---
+++ src/notebook_utils/automation.py
@@ -0,0 +1,48 @@
+import argparse
+import logging
+import nbformat
+import pathlib
+import shlex
+import sys
+import typing
+
+PathLike = typing.TypeVar("PathLike", str, pathlib.Path)
+
+_logger = logging.getLogger(__name__)
+
+
+def strip_to_stdout(notebook_path: PathLike):
+    quoted_path = shlex.quote(str(notebook_path))
+    return f"jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True --stdout {quoted_path} 2>/dev/null"
+
+
+def notebook_is_stripped(notebook_path: PathLike):
+    notebook = nbformat.read(notebook_path, nbformat.NO_CONVERT)
+    for cell in notebook.cells:
+        if cell.cell_type == "code":
+            if len(cell.outputs) != 0:
+                return False
+    return True
+
+
+def notebooks_are_stripped_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-dir",
+        type=pathlib.Path,
+        help="Directory where to recursively look for notebooks. Defaults to the current directory.",
+        default=".",
+    )
+    args = parser.parse_args()
+
+    are_stripped = []
+    for notebook_file in args.dir.rglob("*.ipynb"):
+        if notebook_file.parent.stem != ".ipynb_checkpoints":
+            _logger.info(f"Checking {notebook_file}...")
+            if notebook_is_stripped(notebook_file):
+                are_stripped.append(True)
+            else:
+                are_stripped.append(False)
+                _logger.error(f"Notebook file {notebook_file} is not stripped.")
+
+    sys.exit(not all(are_stripped))
